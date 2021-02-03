871b413565e97805de1a265538e1d27c90587ba9
Josh Rehanek <joshrehanek@gmail.com>
2021-02-03 14:15:08 -0800

    adds id, reaFIle, writeFile, and api routing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,62 @@
 const fs = require('fs');
 const notesDb = require("../db/db.json");
-// const router = require('express').Router();
 const generateUniqueId = require('generate-unique-id');
-const id = generateUniqueId({
-    length: 8,
-    useNumbers: true
-})
+
 
 
 module.exports = function (app) {
 
-    function addNoteToDb(notes) {
-        notes = JSON.stringify(notes);
-        console.log(notes);
-        fs.writeFileSync("./db/db.json", notes, function (err) {
-            if (err) {
-                return console.log(`Error: ${err}`);
-            }
-        });
-    }
+    fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+        if (err) throw err;
+
+        let parsedNote = JSON.parse(data);
+
 
     app.get('/api/notes', (req, res) => {
-        res.json(notesDb);
-    });
+        res.json(parsedNote);
+        res.end();
+    })
 
     app.post('/api/notes', (req, res) => {
         const newNote = req.body;
-        notesDb.push(newNote);
+
+        const noteId = {
+            title: newNote.title,
+            text: newNote.text,
+            id: generateUniqueId({
+                length: 5,
+                useNumbers: true,
+                useLetters: false
+            })
+        };
+
+        notesDb.push(noteId);
         addNoteToDb(notesDb);
         console.log(notesDb);
-        res.json(newNote);
+        res.json(notesDb);
+        res.end();
     });
-   
+
+    app.get('api/notes/:id', (req, res) => {
+        res.json(notesDb[req.params.id]);
+        res.end();
+    })
+
+    app.delete('api/notes/:id', (req, res) => {
+        notesDb.splice(req.params.id, 1);
+        addNoteToDb(notesDb);
+        res.json(notesDb);
+        res.end();
+    })
+})
+
+function addNoteToDb(notes) {
+    notes = JSON.stringify(notes);
+    console.log(notes);
+    fs.writeFileSync("./db/db.json", notes, function (err) {
+        if (err) {
+            return console.log(`Error: ${err}`);
+        }
+    });
+}
 }
