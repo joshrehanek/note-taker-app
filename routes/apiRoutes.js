c215b1eea9d199bccecb8159ead23f386b0eb895
Josh Rehanek <joshrehanek@gmail.com>
2021-02-04 12:14:33 -0800

    moves readFIle inside app.get for cyclical read

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,18 +3,17 @@ const notesDb = require("../db/db.json");
 const generateUniqueId = require('generate-unique-id');
 
 
-
 module.exports = function (app) {
 
-    fs.readFile("./db/db.json", 'utf-8', (err, data) => {
-        if (err) throw err;
-
-        let parsedNote = JSON.parse(data);
-
-
     app.get('/api/notes', (req, res) => {
-        res.json(parsedNote);
-        res.end();
+
+        fs.readFile("./db/db.json", 'utf-8', (err, data) => {
+            if (err) throw err;
+
+            let parsedNote = JSON.parse(data);
+            res.json(parsedNote);
+            
+        })
     })
 
     app.post('/api/notes', (req, res) => {
@@ -32,31 +31,30 @@ module.exports = function (app) {
 
         notesDb.push(noteId);
         addNoteToDb(notesDb);
-        console.log(notesDb);
         res.json(notesDb);
         res.end();
+
     });
 
-    app.get('api/notes/:id', (req, res) => {
-        res.json(notesDb[req.params.id]);
-        res.end();
-    })
-
-    app.delete('api/notes/:id', (req, res) => {
-        notesDb.splice(req.params.id, 1);
+    app.delete('/api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        console.log(`deleting note ${id}`);
+        notesDb.splice(id, 1);
         addNoteToDb(notesDb);
         res.json(notesDb);
         res.end();
-    })
-})
 
-function addNoteToDb(notes) {
-    notes = JSON.stringify(notes);
-    console.log(notes);
-    fs.writeFileSync("./db/db.json", notes, function (err) {
-        if (err) {
-            return console.log(`Error: ${err}`);
-        }
-    });
+    })
+
+
+    function addNoteToDb(notes) {
+        notes = JSON.stringify(notes);
+        console.log(notes);
+        fs.writeFileSync("./db/db.json", notes, function (err) {
+            if (err) {
+                return console.log(`Error: ${err}`);
+            }
+        });
+    }
 }
-}
+
